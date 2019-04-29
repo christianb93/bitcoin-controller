61df7b31f7bac9fd6dd145bf933efad305996f3f
christianb93 <me@unknown>
2019-04-29 12:12:27 +0200

    Changed image pull policy in deployment and fixed run script - use run script for local tests

--- build/controller/minikube_run.sh
+++ build/controller/minikube_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl delete pod bitcoin-controller
-kubectl run bitcoin-controller --image=christianb93/bitcoin-controller --image-pull-policy=Never --restart=Never --serviceaccount='bitcoin-controller-sva' -n bitcoin-controller 
+kubectl delete pod bitcoin-controller -n bitcoin-controller
+kubectl run bitcoin-controller --image=christianb93/bitcoin-controller --image-pull-policy=Never --restart=Never --serviceaccount='bitcoin-controller-sva' -n bitcoin-controller
