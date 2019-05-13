ed6ced02d80e61a39d4718a26fadcbd2584b7c05
Christian <37038420+christianb93@users.noreply.github.com>
2019-05-13 15:05:55 +0200

    Cache kind node docker image

--- travis/promote.sh
+++ travis/promote.sh
@@ -5,6 +5,19 @@
 #
 # Assumes that the following environment variables are set
 # TRAVIS_BUILD_DIR    - directory into which Travis clones the bitcoin-controller-helm-qa repo
+# TRAVIS_HOME         - home directory, we use $TRAVIS_HOME/cache as the cache directory
+
+# 
+# Clean up after test. At this point, our kind cluster should still be running, so we stop it
+#
+kind delete cluster
+# Save docker image if there is no cached instance yet
+if [ -f "$TRAVIS_HOME/cache/kind_node_image.tar" ]; then
+  echo "There is already a version of the kind node image in the cache, not replacing"
+else
+  echo "Unloading kindest/node to cache"
+  docker save --output $TRAVIS_HOME/cache/kind_node_image.tar kindest/node"
+fi
 
 #
 # Get chart version
