79dc49e9f4b4152fa05caf0275bf9bc63f18c6dc
christianb93 <me@unknown>
2020-01-19 17:54:36 +0100

    Added some comments about reproducing the build

--- build/controller/generate_code.sh
+++ build/controller/generate_code.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #
 # First generate the deep copy routine
+# TODO: currently this assumes that you have checked out the generator to the 
+# GOPATH directory
+# Note that the code generation was done with go 1.10 without Go modules enabled and using
+# the commit ff26e7842f9d5224562ef78defed0785ae8395cf from k8s.io/code-generator, as of
+# Fri Apr 19 11:09:55 2019 -0700 (merge of pull request #76796)
+# so if you have problems running this again from scratch you might want to checkout 
+# this version again and use GO111MODULES=off
 #
 go run $GOPATH/src/k8s.io/code-generator/cmd/deepcopy-gen/main.go \
   --bounding-dirs github.com/christianb93/bitcoin-controller/internal/apis \
